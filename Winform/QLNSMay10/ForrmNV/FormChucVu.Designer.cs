5f93131a6841d339046fc651dcb6d39ae8ec9d51
sinhbx <buisinh25042001@gmail.com>
2023-12-15 16:26:37 +0700

    Form chuc vu

--- Winform/QLNSMay10/ForrmNV/FormChucVu.Designer.cs
+++ Winform/QLNSMay10/ForrmNV/FormChucVu.Designer.cs
@@ -30,20 +30,21 @@
         {
             this.components = new System.ComponentModel.Container();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtTenChucVu = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.qLNVDataSet3 = new QLNSMay10.QLNVDataSet3();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.deleteChucVu = new System.Windows.Forms.Button();
+            this.editChucVu = new System.Windows.Forms.Button();
+            this.addChucVu = new System.Windows.Forms.Button();
+            this.grvChucVu = new System.Windows.Forms.DataGridView();
+            this.qLNVChucVu = new QLNSMay10.QLNVChucVu();
             this.chucVuBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.chucVuTableAdapter = new QLNSMay10.QLNVChucVuTableAdapters.ChucVuTableAdapter();
             this.maChucVuDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tenChucVuDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.qLNVDataSet3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.edit = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grvChucVu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLNVChucVu)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chucVuBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,147 +52,157 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(427, 99);
+            this.label2.Location = new System.Drawing.Point(197, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 36);
+            this.label2.Size = new System.Drawing.Size(98, 29);
             this.label2.TabIndex = 2;
             this.label2.Text = "Chức vụ";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox3
+            // txtTenChucVu
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(690, 177);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(151, 30);
-            this.textBox3.TabIndex = 39;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(190, 177);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(151, 30);
-            this.textBox1.TabIndex = 38;
+            this.txtTenChucVu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenChucVu.Location = new System.Drawing.Point(202, 168);
+            this.txtTenChucVu.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTenChucVu.Name = "txtTenChucVu";
+            this.txtTenChucVu.Size = new System.Drawing.Size(114, 26);
+            this.txtTenChucVu.TabIndex = 39;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(599, 177);
+            this.label8.Location = new System.Drawing.Point(133, 168);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(85, 25);
+            this.label8.Size = new System.Drawing.Size(66, 20);
             this.label8.TabIndex = 37;
             this.label8.Text = "Chức vụ";
             // 
-            // label3
+            // deleteChucVu
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(71, 177);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(113, 25);
-            this.label3.TabIndex = 36;
-            this.label3.Text = "Mã chức vụ";
+            this.deleteChucVu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteChucVu.Location = new System.Drawing.Point(357, 228);
+            this.deleteChucVu.Margin = new System.Windows.Forms.Padding(2);
+            this.deleteChucVu.Name = "deleteChucVu";
+            this.deleteChucVu.Size = new System.Drawing.Size(112, 41);
+            this.deleteChucVu.TabIndex = 42;
+            this.deleteChucVu.Text = "Xoá";
+            this.deleteChucVu.UseVisualStyleBackColor = true;
+            this.deleteChucVu.Click += new System.EventHandler(this.deleteChucVu_Click);
             // 
-            // button2
+            // editChucVu
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(744, 280);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 50);
-            this.button2.TabIndex = 42;
-            this.button2.Text = "Xoá";
-            this.button2.UseVisualStyleBackColor = true;
+            this.editChucVu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editChucVu.Location = new System.Drawing.Point(202, 228);
+            this.editChucVu.Margin = new System.Windows.Forms.Padding(2);
+            this.editChucVu.Name = "editChucVu";
+            this.editChucVu.Size = new System.Drawing.Size(112, 41);
+            this.editChucVu.TabIndex = 41;
+            this.editChucVu.Text = "Sửa";
+            this.editChucVu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.editChucVu.UseVisualStyleBackColor = true;
+            this.editChucVu.Click += new System.EventHandler(this.editChucVu_Click);
             // 
-            // button3
+            // addChucVu
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(442, 280);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(150, 50);
-            this.button3.TabIndex = 41;
-            this.button3.Text = "Sửa";
-            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button3.UseVisualStyleBackColor = true;
+            this.addChucVu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addChucVu.Location = new System.Drawing.Point(29, 228);
+            this.addChucVu.Margin = new System.Windows.Forms.Padding(2);
+            this.addChucVu.Name = "addChucVu";
+            this.addChucVu.Size = new System.Drawing.Size(112, 41);
+            this.addChucVu.TabIndex = 40;
+            this.addChucVu.Text = "Thêm";
+            this.addChucVu.UseVisualStyleBackColor = true;
+            this.addChucVu.Click += new System.EventHandler(this.addChucVu_Click);
             // 
-            // button1
+            // grvChucVu
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(122, 280);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 50);
-            this.button1.TabIndex = 40;
-            this.button1.Text = "Thêm";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // qLNVDataSet3
-            // 
-            this.qLNVDataSet3.DataSetName = "QLNVDataSet3";
-            this.qLNVDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeight = 29;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.grvChucVu.AllowUserToAddRows = false;
+            this.grvChucVu.AllowUserToDeleteRows = false;
+            this.grvChucVu.AutoGenerateColumns = false;
+            this.grvChucVu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grvChucVu.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.maChucVuDataGridViewTextBoxColumn,
-            this.tenChucVuDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.chucVuBindingSource;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 425);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(1022, 150);
-            this.dataGridView1.TabIndex = 0;
+            this.tenChucVuDataGridViewTextBoxColumn,
+            this.edit,
+            this.delete});
+            this.grvChucVu.DataSource = this.chucVuBindingSource;
+            this.grvChucVu.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.grvChucVu.Location = new System.Drawing.Point(0, 288);
+            this.grvChucVu.Name = "grvChucVu";
+            this.grvChucVu.ReadOnly = true;
+            this.grvChucVu.Size = new System.Drawing.Size(508, 150);
+            this.grvChucVu.TabIndex = 43;
+            this.grvChucVu.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grvChucVu_CellContentClick);
+            // 
+            // qLNVChucVu
+            // 
+            this.qLNVChucVu.DataSetName = "QLNVChucVu";
+            this.qLNVChucVu.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // chucVuBindingSource
             // 
             this.chucVuBindingSource.DataMember = "ChucVu";
-            this.chucVuBindingSource.DataSource = this.qLNVDataSet3;
+            this.chucVuBindingSource.DataSource = this.qLNVChucVu;
             // 
             // chucVuTableAdapter
             // 
+            this.chucVuTableAdapter.ClearBeforeFill = true;
             // 
             // maChucVuDataGridViewTextBoxColumn
             // 
             this.maChucVuDataGridViewTextBoxColumn.DataPropertyName = "MaChucVu";
-            this.maChucVuDataGridViewTextBoxColumn.HeaderText = "MaChucVu";
-            this.maChucVuDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.maChucVuDataGridViewTextBoxColumn.HeaderText = "Mã chức vụ";
             this.maChucVuDataGridViewTextBoxColumn.Name = "maChucVuDataGridViewTextBoxColumn";
             this.maChucVuDataGridViewTextBoxColumn.ReadOnly = true;
-            this.maChucVuDataGridViewTextBoxColumn.Width = 125;
+            this.maChucVuDataGridViewTextBoxColumn.Visible = false;
             // 
             // tenChucVuDataGridViewTextBoxColumn
             // 
+            this.tenChucVuDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.tenChucVuDataGridViewTextBoxColumn.DataPropertyName = "TenChucVu";
-            this.tenChucVuDataGridViewTextBoxColumn.HeaderText = "TenChucVu";
-            this.tenChucVuDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.tenChucVuDataGridViewTextBoxColumn.HeaderText = "Tên chức vụ";
             this.tenChucVuDataGridViewTextBoxColumn.Name = "tenChucVuDataGridViewTextBoxColumn";
-            this.tenChucVuDataGridViewTextBoxColumn.Width = 125;
+            this.tenChucVuDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // edit
+            // 
+            this.edit.HeaderText = "";
+            this.edit.Name = "edit";
+            this.edit.ReadOnly = true;
+            this.edit.Text = "Chỉnh sửa";
+            this.edit.UseColumnTextForButtonValue = true;
+            // 
+            // delete
+            // 
+            this.delete.HeaderText = "";
+            this.delete.Name = "delete";
+            this.delete.ReadOnly = true;
+            this.delete.Text = "Xóa";
+            this.delete.UseColumnTextForButtonValue = true;
             // 
             // FormChucVu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1022, 575);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(508, 438);
+            this.Controls.Add(this.grvChucVu);
+            this.Controls.Add(this.deleteChucVu);
+            this.Controls.Add(this.editChucVu);
+            this.Controls.Add(this.addChucVu);
+            this.Controls.Add(this.txtTenChucVu);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormChucVu";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FormChucVu_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.qLNVDataSet3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            //((System.ComponentModel.ISupportInitialize)(this.chucVuBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grvChucVu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLNVChucVu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chucVuBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -200,17 +211,18 @@
         #endregion
 
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtTenChucVu;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button1;
-        private QLNVDataSet3 qLNVDataSet3;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button deleteChucVu;
+        private System.Windows.Forms.Button editChucVu;
+        private System.Windows.Forms.Button addChucVu;
+        private System.Windows.Forms.DataGridView grvChucVu;
+        private QLNVChucVu qLNVChucVu;
+        private System.Windows.Forms.BindingSource chucVuBindingSource;
+        private QLNVChucVuTableAdapters.ChucVuTableAdapter chucVuTableAdapter;
         private System.Windows.Forms.DataGridViewTextBoxColumn maChucVuDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn tenChucVuDataGridViewTextBoxColumn;
-        private System.Windows.Forms.BindingSource chucVuBindingSource;
+        private System.Windows.Forms.DataGridViewButtonColumn edit;
+        private System.Windows.Forms.DataGridViewButtonColumn delete;
     }
 }
--- Winform/QLNSMay10/ForrmNV/FormChucVu.cs
+++ Winform/QLNSMay10/ForrmNV/FormChucVu.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using QLNSMay10.ConnectSql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,19 +14,117 @@ namespace QLNSMay10.ForrmNV
 {
     public partial class FormChucVu : Form
     {
+        QLNVEntities qlnv = new QLNVEntities();
+        BindingSource bd = new BindingSource();
+        int checkId = 0;
         public FormChucVu()
         {
             InitializeComponent();
         }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void FormChucVu_Load(object sender, EventArgs e)
         {
+            this.chucVuTableAdapter.Fill(this.qLNVChucVu.ChucVu);
+        }
 
+        private void addChucVu_Click(object sender, EventArgs e)
+        {
+            string tenchucvu = txtTenChucVu.Text.Trim();
+            if (tenchucvu == "")
+            {
+                MessageBox.Show("Không để trống trường nào!", "Thông báo",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                var listChucvu = qlnv.ChucVus.ToList();
+                var result = listChucvu.Find(item => item.TenChucVu.Equals(tenchucvu));
+                if (result != null)
+                {
+                    MessageBox.Show("Đã có chức vụ này rồi mời nhập lại!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    ChucVu chucvu = new ChucVu();
+                    chucvu.TenChucVu = tenchucvu;
+                    qlnv.ChucVus.Add(chucvu);
+                    qlnv.SaveChanges();
+                    bd.DataSource = qlnv.ChucVus.ToList();
+                    grvChucVu.DataSource = bd.DataSource;
+                    grvChucVu.Refresh();
+                    MessageBox.Show("Thêm mới thành công! ", "Thông báo",
+                     MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtTenChucVu.Text = "";
+                }
+
+
+            }
+        }
+
+        private void deleteChucVu_Click(object sender, EventArgs e)
+        {
+            txtTenChucVu.Text = "";
+        }
+
+        private void grvChucVu_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            checkId = Int32.Parse(grvChucVu.Rows[e.RowIndex].Cells[0].Value.ToString());
+            if (e.RowIndex >= 0 && grvChucVu.Columns[e.ColumnIndex].Name == "edit")
+            {
+                if (e.RowIndex < grvChucVu.Rows.Count && grvChucVu.SelectedCells.Count > 0)
+                {
+
+                    txtTenChucVu.Text = grvChucVu.Rows[e.RowIndex].Cells[1].Value.ToString();
+              
+                }
+            }
+            if (e.RowIndex >= 0 && grvChucVu.Columns[e.ColumnIndex].Name == "delete")
+            {
+                if (e.RowIndex < grvChucVu.Rows.Count && grvChucVu.SelectedCells.Count > 0)
+                {
+                    DialogResult result = MessageBox.Show("Bạn chắc chắn muốn xóa hàng này này chứ?", "Thông báo",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (result == DialogResult.Yes)
+                    {
+                       
+
+                        var chucvu= qlnv.ChucVus.SingleOrDefault(item => item.MaChucVu == checkId);
+                        Console.WriteLine("aaaa " + JsonConvert.SerializeObject(chucvu, Formatting.Indented));
+                        if (chucvu != null)
+                        {
+                            MessageBox.Show("Xóa thành công! ", "Thông báo",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            qlnv.ChucVus.Remove(chucvu);
+                            qlnv.SaveChanges();
+                            bd.DataSource = qlnv.ChucVus.ToList();
+                            grvChucVu.DataSource = bd.DataSource;
+                            grvChucVu.Refresh();
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void editChucVu_Click(object sender, EventArgs e)
+        {
+            var chucvu = qlnv.ChucVus.SingleOrDefault(item => item.MaChucVu == checkId);
+            if (txtTenChucVu.Text == "")
+            {
+                MessageBox.Show("Không để trống trường nào!", "Thông báo",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                chucvu.TenChucVu = txtTenChucVu.Text;
+                qlnv.SaveChanges();
+                MessageBox.Show("Chỉnh sửa thành công! ", "Thông báo",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                bd.DataSource = qlnv.ChucVus.ToList();
+                grvChucVu.DataSource = bd.DataSource;
+                grvChucVu.Refresh();
+            }
         }
     }
 }
